e4db4632acb7c45fdabcf749832d79b1d02116cd
owlwilderness.eth <98717833+OwlWilderness@users.noreply.github.com>
2023-04-24 22:15:45 -0400

    Revert "deploy to goerli - dont push"
    This reverts commit 5507428f581fc86d8d7a5848de8dfa4957567482.

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,8 +1,8 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "cbddb2444063488aa4f366e174d54a9e"; // trying the  emoji.support key
+export const INFURA_ID = "5b3aa68d82264f59bb6a1874cb3c23ea"; // trying the  emoji.support key
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
-export const ETHERSCAN_KEY = "WQ1ZUN9MR2ZBREBFB9JKWRHF78QVCK728C";
+export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
 
 // BLOCKNATIVE ID FOR Notify.js:
 export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
